6f37366bda9a4a533640d6a9fd291813c2bec754
braisslor <2kirkbrown@gmail.com>
2014-05-15 14:54:17 -0500

    1.0 plottwatt API Complete
    This is a major update. The functions are all working as far as I know.
    The example sketches can be used to list the meters, add meters and
    delete meters. Once you have the meters set up correctly, you can update
    the information in basicPlottWatt.ino to make it work for your account.

--- PlotWatt.h
+++ PlotWatt.h
@@ -1,5 +1,5 @@
 /*
-  PlottWatt.h
+  PlotWatt.h
   
   This library will include ethernet functionality, emon lib functionality, and
   the structure for posting data to plottwatt. I did not write very much of this code myself.
@@ -7,10 +7,24 @@
   notices alone as best I can.
   
 */
+#include <Ethernet.h>
+#ifndef PlotWatt_h
+#define PlotWatt_h
 
-#ifndef PlottWatt_h
-#define PlottWatt_h
 
+
+
+//These are the raw commands for communication
+#define SERVERNAME "www.plotwatt.com"
+#define POSTREADINGS1 "POST /api/v2/push_readings HTTP/1.1\r\nAuthorization: Basic "
+#define POSTREADINGS2 "\r\nHost: plotwatt.com\r\nContent-Length: "
+#define LISTMETERS1 "GET /api/v2/list_meters HTTP/1.1\r\nAuthorization: Basic "
+#define LISTMETERS2 "\r\nHost: plotwatt.com\r\n\r\n"
+#define DELETEMETER1 "PUT /api/v2/delete_meter HTTP/1.1\r\nAuthorization: Basic "
+#define DELETEMETER2 "\r\nHost: plotwatt.com\r\nContent-Length: "
+#define DELETEMETER3 "\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\nmeter_id="
+#define ADDMETERS1 "POST /api/v2/new_meters HTTP/1.1\r\nAuthorization: Basic "
+#define ADDMETERS2 "\r\nHost: plotwatt.com\r\nContent-Length: "
 #if defined(ARDUINO) && ARDUINO >= 100
 
 #include "Arduino.h"
@@ -21,7 +35,31 @@
 
 #endif
 
+class PlotWatt {
+	public:
+		PlotWatt(EthernetClient& aClient, const char* apiKey, int sizeOfArray, long _meterNumber1, long _meterNumber2);	
+		PlotWatt(EthernetClient& aClient, const char* apiKey); //Used for listMeters
+		void listMeters();
+		void addMeter(int _meters);
+		void deleteMeter(long _meter);
+		void pushData(float *_power1, unsigned long *_timeArray1, float *_power2, unsigned long *_timeArray2);
+		
 
+		
+	private:
+
+		int calculateLength(unsigned long *_object);
+		int calculateLength(long _meter);
+		int calculateLength( float *_object);
+		char serverName[];
+		void readDelay(byte delayTimes);
+		//Create a EthernetClient that will perform telnet operations
+		EthernetClient _client;	
+		const char* _apiKey;
+		long _meterNumber1;
+		long _meterNumber2;
+		int _sizeOfArray;
+};
 
 
 
